5cd11d40940f5216a883212cdca37f0795f837f1
oldrex <835708122@qq.com>
2023-07-22 22:45:08 +0800

    add mysql

--- be/src/main/java/com/mile/one/oneMile/mongo/AccessingMongodbDataRestApplication.java
+++ be/src/main/java/com/mile/one/oneMile/mongo/AccessingMongodbDataRestApplication.java
@@ -3,7 +3,7 @@ package com.mile.one.oneMile.mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class AccessingMongodbDataRestApplication {
 
     public static void main(String[] args) {
